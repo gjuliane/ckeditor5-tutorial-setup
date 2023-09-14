9ed19e6b9da9d04bf03f9d0a8191b71300b470a5
Gustavo Julian <gjuliane@gmail.com>
2023-09-14 16:20:37 -0600

    profiler

--- src/main.js
+++ src/main.js
@@ -1,18 +1,35 @@
 import { ClassicEditor } from "@ckeditor/ckeditor5-editor-classic";
 import { Essentials } from "@ckeditor/ckeditor5-essentials";
+// Essentials is wraper for Clipboard, Enter, SelectAll, ShiftEnter, Typing, Undo
 import { Paragraph } from "@ckeditor/ckeditor5-paragraph";
+import { RemoveFormat } from "@ckeditor/ckeditor5-remove-format";
+import { Bold, Italic } from '@ckeditor/ckeditor5-basic-styles';
+
+import CKEditorInspector from '@ckeditor/ckeditor5-inspector';
+
+
 
 const element = document.querySelector('#app');
 
 const editor = await ClassicEditor.create(element, {
     plugins: [
         Essentials, // undo, redo
-        Paragraph
+        Paragraph,
+        RemoveFormat,
+        Bold, Italic
     ],
     toolbar: [
         'undo',
-        'redo'
+        'redo',
+        'removeFormat',
+        'bold', 'italic'
     ]
-});
+} )
+.then( editor => {
+    CKEditorInspector.attach( editor );
+} )
+.catch( error => {
+    console.error( error );
+} );
 
-window.editor = editor;
+window.editor = editor; // for debbuging
---
+++ src/plugin.js
@@ -0,0 +1,5 @@
+// import { ClassicEditor } from "@ckeditor/ckeditor5-editor-classic";
+
+// const element = document.querySelector('#app');
+
+// const editor = await ClassicEditor.create(element);
